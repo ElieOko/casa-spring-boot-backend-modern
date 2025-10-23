189b8cc29c2dfc9af687671e8928692f7d93638c
ElieOko <elieoko100@gmail.com>
2025-10-23 17:21:32 +0100

    fix:refactor

--- src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/PropertyRepository.kt
+++ src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/PropertyRepository.kt
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository
 import server.web.casa.app.property.infrastructure.persistence.entity.PropertyEntity
 
 interface PropertyRepository : JpaRepository<PropertyEntity, Long> {
-    @EntityGraph(attributePaths = ["propertyImage","propertyImageRoom","propertyImageLivingRoom","propertyImageKitchen"])
+    @EntityGraph(attributePaths = [
+        "propertyImage",
+        "propertyImageRoom",
+        "propertyImageLivingRoom",
+        "propertyImageKitchen"])
     override fun findAll(): List<PropertyEntity>
 }
