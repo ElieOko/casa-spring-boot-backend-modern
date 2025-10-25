73ff5c3f483079ae2938e54310ea3c7559b832f5
ElieOko <elieoko100@gmail.com>
2025-10-25 14:17:16 +0100

    feat:app address replace jpaRepository to coroutine repository

--- src/main/kotlin/server/web/casa/app/address/infrastructure/persistence/repository/QuartierRepository.kt
+++ src/main/kotlin/server/web/casa/app/address/infrastructure/persistence/repository/QuartierRepository.kt
@@ -1,7 +1,7 @@
 package server.web.casa.app.address.infrastructure.persistence.repository
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import server.web.casa.app.address.infrastructure.persistence.entity.QuartierEntity
 
-interface QuartierRepository : JpaRepository<QuartierEntity,Long> {
+interface QuartierRepository : CoroutineCrudRepository<QuartierEntity,Long> {
 }
