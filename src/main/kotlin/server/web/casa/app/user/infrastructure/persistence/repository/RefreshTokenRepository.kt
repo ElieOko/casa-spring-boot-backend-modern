e7d19a5c733721371770c2d6ae5d9364296cf76b
ElieOko <elieoko100@gmail.com>
2025-10-25 14:13:33 +0100

    feat:app user replace jpaRepository to coroutine repository

--- src/main/kotlin/server/web/casa/app/user/infrastructure/persistence/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/server/web/casa/app/user/infrastructure/persistence/repository/RefreshTokenRepository.kt
@@ -1,9 +1,9 @@
 package server.web.casa.app.user.infrastructure.persistence.repository
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import server.web.casa.app.user.infrastructure.persistence.entity.RefreshToken
 
-interface RefreshTokenRepository : JpaRepository<RefreshToken, Long> {
-    fun findByUserIdAndHashedToken(userId: Long, hashedToken: String): RefreshToken?
-    fun deleteByUserIdAndHashedToken(userId: Long, hashedToken: String)
+interface RefreshTokenRepository : CoroutineCrudRepository<RefreshToken, Long> {
+    suspend fun findByUserIdAndHashedToken(userId: Long, hashedToken: String): RefreshToken?
+    suspend fun deleteByUserIdAndHashedToken(userId: Long, hashedToken: String)
 }
--- src/main/kotlin/server/web/casa/app/user/infrastructure/persistence/repository/TypeAccountRepository.kt
+++ src/main/kotlin/server/web/casa/app/user/infrastructure/persistence/repository/TypeAccountRepository.kt
@@ -1,7 +1,7 @@
 package server.web.casa.app.user.infrastructure.persistence.repository
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import server.web.casa.app.user.infrastructure.persistence.entity.TypeAccountEntity
 
-interface TypeAccountRepository : JpaRepository<TypeAccountEntity, Long> {
+interface TypeAccountRepository : CoroutineCrudRepository<TypeAccountEntity, Long> {
 }
