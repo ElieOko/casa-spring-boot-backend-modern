f74a871bcb500dc7b3f3a8245b0c4bf9f4d6219a
ElieOko <elieoko100@gmail.com>
2025-10-27 13:57:37 +0100

    fix:review encodage pour la production

--- src/main/kotlin/server/web/casa/utils/FileEncodage.kt
+++ src/main/kotlin/server/web/casa/utils/FileEncodage.kt
@@ -1,27 +1,53 @@
 package server.web.casa.utils
 
-import org.springframework.mock.web.MockMultipartFile
+import org.springframework.web.multipart.MultipartFile
 import java.io.File
-import java.lang.Exception
-import java.time.LocalDateTime
 import java.util.Base64
+import java.io.*
 
-fun convertFile(base64 : String): File {
-    val decodedBytes = Base64.getDecoder().decode(base64)
-    val filePath = "image_${LocalDateTime.now().second}_.png"
-    val outputFile = File(filePath)
-    outputFile.writeBytes(decodedBytes)
-    return outputFile
+class Base64DecodedMultipartFile(
+    private val content: ByteArray,
+    private val headerName: String,
+    private val originalFileName: String,
+    private val mimeType: String
+) : MultipartFile {
+
+    override fun getName(): String = headerName
+    override fun getOriginalFilename(): String = originalFileName
+    override fun getContentType(): String = mimeType
+    override fun isEmpty(): Boolean = content.isEmpty()
+    override fun getSize(): Long = content.size.toLong()
+    override fun getBytes(): ByteArray = content
+    override fun getInputStream(): InputStream = ByteArrayInputStream(content)
+
+    override fun transferTo(dest: File) {
+        FileOutputStream(dest).use { it.write(content) }
+    }
 }
+
+
+//fun base64ToMultipartFile(
+//    base64: String,
+//    prefix : String ="",
+//    fieldName: String = "file",
+//    mimeType: String = "image/png"
+//): Base64DecodedMultipartFile {
+//    val timestamp = System.currentTimeMillis()
+//    val fileName: String = "image_${prefix}_${timestamp}.png"
+//    val imageBytes = Base64.getDecoder().decode(base64)
+//
+//    return Base64DecodedMultipartFile(fieldName, fileName, mimeType, imageBytes)
+//}
 
 fun base64ToMultipartFile(
     base64: String,
-    prefix : String ="",
+    prefix: String = "",
     fieldName: String = "file",
     mimeType: String = "image/png"
-): MockMultipartFile {
+): MultipartFile {
     val timestamp = System.currentTimeMillis()
-    val fileName: String = "image_${prefix}_${timestamp}.png"
+    val fileName = "image_${prefix}_${timestamp}.png"
     val imageBytes = Base64.getDecoder().decode(base64)
-    return MockMultipartFile(fieldName, fileName, mimeType, imageBytes)
+
+    return Base64DecodedMultipartFile(imageBytes, fieldName, fileName, mimeType)
 }
