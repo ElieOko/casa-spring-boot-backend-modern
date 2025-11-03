ce02c2c25439019affe2c4c92fd83b7f24717781
ElieOko <elieoko100@gmail.com>
2025-11-03 16:32:03 +0100

    fix:modele de profile

---
+++ src/main/kotlin/server/web/casa/app/user/domain/model/Profil.kt
@@ -0,0 +1,12 @@
+package server.web.casa.app.user.domain.model
+
+data class ProfileUser(
+    var firstname : String?="",
+    var lastname : String?="",
+    var fullname   : String?="",
+    var address    : String?="",
+    var images     : String?="",
+    var cardFront  : String?="",
+    var cardBack   : String?="",
+    var numberCard : String?=""
+)
