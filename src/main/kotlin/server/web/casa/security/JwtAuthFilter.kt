a79f3e64f0312b422cfd7a446ae17cc5873c0c93
ElieOko <elieoko100@gmail.com>
2025-11-05 15:39:28 +0100

    fix:get userId

--- src/main/kotlin/server/web/casa/security/JwtAuthFilter.kt
+++ src/main/kotlin/server/web/casa/security/JwtAuthFilter.kt
@@ -3,6 +3,7 @@ package server.web.casa.security
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
@@ -15,7 +16,7 @@ import server.web.casa.utils.Mode
 class JwtAuthFilter(
     private val jwtService: JwtService
 ): OncePerRequestFilter() {
-
+    private val log = LoggerFactory.getLogger(this::class.java)
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -25,6 +26,7 @@ class JwtAuthFilter(
         if(authHeader != null && authHeader.startsWith("Bearer ")) {
             if(jwtService.validateAccessToken(authHeader)) {
                 val userId = jwtService.getUserIdFromToken(authHeader)
+                log.info("jwt userId -> $userId")
                 val auth = UsernamePasswordAuthenticationToken(userId, null, emptyList())
                 SecurityContextHolder.getContext().authentication = auth
             }
