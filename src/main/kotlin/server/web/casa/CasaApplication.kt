59e42eef8fca577751d1999cdc29413b39d9fa7d
ElieOko <elieoko100@gmail.com>
2025-12-09 09:17:18 +0100

    fix:add propertie

--- src/main/kotlin/server/web/casa/CasaApplication.kt
+++ src/main/kotlin/server/web/casa/CasaApplication.kt
@@ -2,14 +2,17 @@ package server.web.casa
 
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import server.web.casa.utils.Mode
+import server.web.casa.utils.storage.StorageProperties
 
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties::class)
 @Profile(Mode.DEV)
 class CasaApplication
 
