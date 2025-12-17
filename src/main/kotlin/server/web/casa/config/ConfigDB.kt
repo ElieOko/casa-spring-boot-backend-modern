3263fb85691429738c56450725d652394ae0104c
ElieOko <elieoko100@gmail.com>
2025-12-17 11:13:23 +0100

    fix:config database to flyaway

---
+++ src/main/kotlin/server/web/casa/config/ConfigDB.kt
@@ -0,0 +1,34 @@
+package server.web.casa.config
+
+
+import org.flywaydb.core.Flyway
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.flyway.autoconfigure.FlywayProperties
+import org.springframework.boot.r2dbc.autoconfigure.R2dbcProperties
+
+@Configuration
+@EnableConfigurationProperties(
+    FlywayProperties::class,
+    R2dbcProperties::class
+)
+class DatabaseConfig {
+
+    @Bean(initMethod = "migrate")
+    fun flyway(
+        flywayProperties: FlywayProperties,
+        r2dbcProperties: R2dbcProperties
+    ): Flyway {
+
+        return Flyway.configure()
+            .dataSource(
+                "jdbc:postgresql://aws-1-eu-west-1.pooler.supabase.com:5432/postgres",          // JDBC URL
+                r2dbcProperties.username,
+                r2dbcProperties.password
+            )
+            .locations(*flywayProperties.locations.toTypedArray())
+            .baselineOnMigrate(true)
+            .load()
+    }
+}
