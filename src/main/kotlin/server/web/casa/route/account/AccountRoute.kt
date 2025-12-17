1dcbe24c22dff35d4670d8b53dcf2ddda46821f7
ElieOko <elieoko100@gmail.com>
2025-12-17 12:26:07 +0100

    fix:add new route account

---
+++ src/main/kotlin/server/web/casa/route/account/AccountRoute.kt
@@ -0,0 +1,13 @@
+package server.web.casa.route.account
+
+import server.web.casa.route.GlobalRoute
+
+object AccountRoute {
+    const val ACCOUNT = "${GlobalRoute.ROOT}/${AccountFeatures.ACCOUNT_PATH}"
+    const val ACCOUNT_TYPE = "${GlobalRoute.ROOT}/${AccountFeatures.ACCOUNT_TYPE_PATH}"
+}
+
+object AccountFeatures {
+    const val ACCOUNT_PATH = "accounts"
+    const val ACCOUNT_TYPE_PATH = "accounts/type"
+}
