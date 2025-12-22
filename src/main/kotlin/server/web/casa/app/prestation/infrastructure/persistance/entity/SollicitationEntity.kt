3691e14bedf11349fe2b8f5ab42cc0256a81f20b
mr zou <a.tshiyanze@gmail.com>
2025-12-22 19:25:19 +0100

    entity et model prestation - sollicitation

---
+++ src/main/kotlin/server/web/casa/app/prestation/infrastructure/persistance/entity/SollicitationEntity.kt
@@ -0,0 +1,38 @@
+package server.web.casa.app.prestation.infrastructure.persistance.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDate
+
+@Table("sollicitations")
+data class SollicitationEntity(
+    @Id
+    @Column("id")
+    val id: Long? = null,
+
+    @Column("user_id")
+    val userId: Long?,
+
+    @Column("prestation_id")
+    val prestationId: Long?,
+
+    @Column("devise_id")
+    val deviseId: Long?,
+
+    @Column("budget")
+    val budegt: Double?,
+
+    @Column("description")
+    val description: String,
+
+    @Column("start_date")
+    val startDate: LocalDate,
+
+    @Column("end_date")
+    val endDate: LocalDate,
+
+    @Column("created_at")
+    val createdAt: LocalDate = LocalDate.now()
+
+)
