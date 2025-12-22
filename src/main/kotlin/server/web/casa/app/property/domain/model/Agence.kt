754e8246b59de895ad5f8dbcb8e1f11c8f41b707
ElieOko <elieoko100@gmail.com>
2025-12-22 09:56:43 +0100

    fix:structuration model conjoint

---
+++ src/main/kotlin/server/web/casa/app/property/domain/model/Agence.kt
@@ -0,0 +1,25 @@
+package server.web.casa.app.property.domain.model
+
+import server.web.casa.app.property.infrastructure.persistence.entity.AgenceEntity
+
+class Agence(
+    val id: Long? = null,
+    val userId: Long,
+    val name: String,
+    val description : String = "",
+    val phone1 : String = "",
+    val phone2 : String = "",
+    val address : String = "",
+    val logo : String = "",
+)
+
+fun Agence.toEntity() = AgenceEntity(
+    this.id,
+    this.userId,
+    this.name,
+    this.description,
+    this.phone1,
+    this.phone2,
+    this.address,
+    this.logo
+)
