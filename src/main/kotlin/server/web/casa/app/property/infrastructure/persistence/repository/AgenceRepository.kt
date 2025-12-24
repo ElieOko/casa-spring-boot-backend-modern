dd72d90e486114102b7800ea876ea67cb8c89f18
ElieOko <elieoko100@gmail.com>
2025-12-24 11:14:07 +0100

    fix:new query for byUser

--- src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/AgenceRepository.kt
+++ src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/AgenceRepository.kt
@@ -1,5 +1,6 @@
 package server.web.casa.app.property.infrastructure.persistence.repository
 
+import kotlinx.coroutines.flow.Flow
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import server.web.casa.app.property.infrastructure.persistence.entity.AgenceEntity
@@ -7,4 +8,7 @@ import server.web.casa.app.property.infrastructure.persistence.entity.AgenceEnti
 interface AgenceRepository : CoroutineCrudRepository<AgenceEntity, Long>{
     @Query("SELECT COUNT(*) FROM agences WHERE user_id = :userId")
     suspend fun countByUserId(userId: Long) : Long
+
+    @Query("SELECT * FROM agences WHERE user_id = :userId")
+    suspend fun getAllByUser(userId: Long) : Flow<AgenceEntity?>
 }
