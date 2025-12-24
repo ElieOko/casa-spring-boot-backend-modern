554c7bf28855d54568f379c62aea0e92c2ada450
ElieOko <elieoko100@gmail.com>
2025-12-24 13:09:30 +0100

    fix:new query group by in

--- src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/VacanceImageRepository.kt
+++ src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/VacanceImageRepository.kt
@@ -1,9 +1,11 @@
 package server.web.casa.app.property.infrastructure.persistence.repository
 
+import kotlinx.coroutines.flow.Flow
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import server.web.casa.app.property.infrastructure.persistence.entity.BureauImageEntity
 import server.web.casa.app.property.infrastructure.persistence.entity.VacanceEntity
 import server.web.casa.app.property.infrastructure.persistence.entity.VacanceImageEntity
 
 interface VacanceImageRepository : CoroutineCrudRepository<VacanceImageEntity, Long>{
-
+    fun findByVacanceIdIn(vacanceIds: List<Long>): Flow<VacanceImageEntity>
 }
