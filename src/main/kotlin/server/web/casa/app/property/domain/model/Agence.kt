8076f882eb028d5f963132626e9343c93ffda4f7
ElieOko <elieoko100@gmail.com>
2025-12-24 11:13:36 +0100

    fix:add annotation to model

--- src/main/kotlin/server/web/casa/app/property/domain/model/Agence.kt
+++ src/main/kotlin/server/web/casa/app/property/domain/model/Agence.kt
@@ -1,15 +1,25 @@
 package server.web.casa.app.property.domain.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Null
 import server.web.casa.app.property.infrastructure.persistence.entity.AgenceEntity
 
 class Agence(
+    @Null
+    @JsonIgnore
     val id: Long? = null,
+    @NotNull
     val userId: Long,
+    @NotNull
     val name: String,
     val description : String = "",
+    @NotNull
     val phone1 : String = "",
     val phone2 : String = "",
+    @NotNull
     val address : String = "",
+    @NotNull
     var logo : String? = "",
 )
 
