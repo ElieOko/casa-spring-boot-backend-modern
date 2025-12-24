3ee0a88767e4ee1a8b186cc63a79e7eda51c327d
ElieOko <elieoko100@gmail.com>
2025-12-24 15:07:37 +0100

    fix:new routes

--- src/main/kotlin/server/web/casa/route/utils/AgenceRoute.kt
+++ src/main/kotlin/server/web/casa/route/utils/AgenceRoute.kt
@@ -2,5 +2,11 @@ package server.web.casa.route.utils
 
 import server.web.casa.route.GlobalRoute
 
-object AgenceRoute { const val AGENCE = "${GlobalRoute.ROOT}/${AgenceFeature.AGENCE_PATH}" }
-object AgenceFeature { const val AGENCE_PATH = "agences" }
+object AgenceRoute {
+    const val AGENCE = "${GlobalRoute.ROOT}/${AgenceFeature.AGENCE_PATH}"
+    const val CARD_TYPE = "${GlobalRoute.ROOT}/${AgenceFeature.CARD_PATH}"
+}
+object AgenceFeature {
+    const val AGENCE_PATH = "agences"
+    const val CARD_PATH = "cards"
+}
