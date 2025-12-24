925e80ca91671622e3d85c36b991fde4651de281
ElieOko <elieoko100@gmail.com>
2025-12-24 13:10:03 +0100

    fix:review model

--- src/main/kotlin/server/web/casa/app/property/domain/model/BureauImage.kt
+++ src/main/kotlin/server/web/casa/app/property/domain/model/BureauImage.kt
@@ -1,15 +1,20 @@
 package server.web.casa.app.property.domain.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import server.web.casa.app.property.infrastructure.persistence.entity.BureauImageEntity
 import server.web.casa.app.property.infrastructure.persistence.entity.VacanceImageEntity
 import java.time.LocalDateTime
 
 data class BureauImage(
+    @JsonIgnore
     val id: Long? = null,
+    @JsonIgnore
     val bureauId : Long,
     val name: String = "",
     val path: String? = "",
+    @JsonIgnore
     var isAvailable: Boolean = true,
+    @JsonIgnore
     val createdAt: LocalDateTime = LocalDateTime.now(),
 )
 
