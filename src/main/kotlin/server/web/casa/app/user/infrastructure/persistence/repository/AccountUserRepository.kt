c0b76392de536d48415ba1f4d1a83a2d5e533096
ElieOko <elieoko100@gmail.com>
2025-12-26 11:09:50 +0100

    fix:review repository rename

--- src/main/kotlin/server/web/casa/app/user/infrastructure/persistence/repository/AccountUserRepository.kt
+++ src/main/kotlin/server/web/casa/app/user/infrastructure/persistence/repository/AccountUserRepository.kt
@@ -3,12 +3,10 @@ package server.web.casa.app.user.infrastructure.persistence.repository
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import server.web.casa.app.ecosystem.infrastructure.persistence.entity.PrestationEntity
-import server.web.casa.app.property.infrastructure.persistence.entity.PropertyFeatureEntity
-import server.web.casa.app.user.infrastructure.persistence.entity.TypeAccountUserEntity
+import server.web.casa.app.user.infrastructure.persistence.entity.AccountUserEntity
 
-interface TypeAccountUserRepository : CoroutineCrudRepository<TypeAccountUserEntity, Long> {
-    @Query("SELECT * FROM type_account_users WHERE user_id = :userId AND type_account_id = :typeAccountId")
-    suspend fun findByUserAndAccount(userId: Long, typeAccountId : Long) : TypeAccountUserEntity?
-    fun findByUserIdIn(userIds: List<Long>): Flow<TypeAccountUserEntity>
+interface AccountUserRepository : CoroutineCrudRepository<AccountUserEntity, Long> {
+    @Query("SELECT * FROM account_users WHERE user_id = :userId AND account_id = :accountId")
+    suspend fun findByUserAndAccount(userId: Long, accountId : Long) : AccountUserEntity?
+    fun findByUserIdIn(userIds: List<Long>): Flow<AccountUserEntity>
 }
