4fec793025e0d4f363c829d04698d0208a2e651d
mr zou <a.tshiyanze@gmail.com>
2025-12-27 19:31:41 +0100

    update favorite to DTO

--- src/main/kotlin/server/web/casa/app/property/domain/model/Favorite.kt
+++ src/main/kotlin/server/web/casa/app/property/domain/model/Favorite.kt
@@ -1,5 +1,7 @@
 package server.web.casa.app.property.domain.model
 
+import server.web.casa.app.property.domain.model.dto.PropertyMasterDTO
+import server.web.casa.app.property.infrastructure.persistence.entity.FavoriteEntity
 import server.web.casa.app.user.domain.model.UserDto
 import java.time.LocalDate
 
@@ -8,4 +10,10 @@ data class Favorite(
     val userId: Long?,
     val propertyId: Long?,
     val createdAt: LocalDate
+)
+
+data class FavoriteDTO(
+    val favorite: FavoriteEntity?,
+    val property: PropertyMasterDTO,
+
 )
