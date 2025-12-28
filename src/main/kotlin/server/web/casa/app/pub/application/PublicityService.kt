6738f3ef6f992f9c146ed8530c9a35a0f5b7d973
mr zou <a.tshiyanze@gmail.com>
2025-12-28 11:00:09 +0100

    update serice pub for upload image base 64

--- src/main/kotlin/server/web/casa/app/pub/application/PublicityService.kt
+++ src/main/kotlin/server/web/casa/app/pub/application/PublicityService.kt
@@ -3,20 +3,32 @@ package server.web.casa.app.pub.application
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 import org.threeten.bp.LocalTime
 import server.web.casa.app.pub.infrastructure.persistance.entity.PublicityEntity
 import server.web.casa.app.pub.infrastructure.persistance.repository.PublicityRepository
 import server.web.casa.utils.Mode
+import server.web.casa.utils.base64ToMultipartFile
+import server.web.casa.utils.gcs.GcsService
 import java.time.LocalDate
 
 @Service
 @Profile(Mode.DEV)
 class PublicityService(
-    private  val repo: PublicityRepository
+    private  val repo: PublicityRepository,
+    private val gcsService: GcsService
 ) {
+    private val log = LoggerFactory.getLogger(this::class.java)
+    private val subdirectory = "pub/"
     suspend fun createPub(pub: PublicityEntity): PublicityEntity {
+        val file = base64ToMultipartFile(pub.imagePath!!, "pub")
+        log.info("file ****taille:${file.size}")
+        log.info("file ****name:${file.name}")
+        val imageUrl = gcsService.uploadFile(file,subdirectory)
+        log.info("public url local ${imageUrl}")
+        pub.imagePath = imageUrl
         return repo.save(pub)
     }
     suspend fun findAllPub() = repo.findAll().map{ it }.toList()
