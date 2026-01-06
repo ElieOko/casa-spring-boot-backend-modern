bd3d8ba252ebc38a208ffcfc567c9525d9306e5e
ElieOko <elieoko100@gmail.com>
2026-01-06 23:32:14 +0100

    feat:revue des methodes au niveau du controller

--- src/main/kotlin/server/web/casa/app/prestation/infrastructure/controller/PrestationFavoriteController.kt
+++ src/main/kotlin/server/web/casa/app/prestation/infrastructure/controller/PrestationFavoriteController.kt
@@ -29,7 +29,7 @@ class PrestationFavoriteController(
 ) {
 
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE])
-    suspend fun create(
+    suspend fun createPrestationFavorite(
         @Valid @RequestBody request: FavoritePrestationRequest
     ): ResponseEntity<Map<String, Any?>>{
         val user = userS.findIdUser(request.userId)
@@ -50,24 +50,24 @@ class PrestationFavoriteController(
     }
 
     @GetMapping("/",produces = [MediaType.APPLICATION_JSON_VALUE])
-    suspend fun getAll():ResponseEntity<Map<String,List<FavoritePrestationDTO>>>{
+    suspend fun getAllFavortitePrestation():ResponseEntity<Map<String,List<FavoritePrestationDTO>>>{
         val all = service.findAll()
         return ResponseEntity.ok().body(mapOf("data" to all))
     }
 
     @GetMapping("/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    suspend fun getReservationById(@PathVariable id: Long): ResponseEntity<Map<String, FavoritePrestationDTO?>>{
+    suspend fun getReservationPrestationById(@PathVariable id: Long): ResponseEntity<Map<String, FavoritePrestationDTO?>>{
         val find = service.findById(id)
         return ResponseEntity.ok().body(mapOf("data" to find))
     }
 
     @GetMapping("/{userId}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    suspend fun getByUser(@PathVariable userId: Long): ResponseEntity<Map<String, List<FavoritePrestationDTO>?>>{
+    suspend fun getByUserPrestation(@PathVariable userId: Long): ResponseEntity<Map<String, List<FavoritePrestationDTO>?>>{
         val find = service.findByUserId( userId)
         return ResponseEntity.ok(mapOf("data" to find))
     }
     @GetMapping("/{prestationId}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    suspend fun getByPrestation(@PathVariable prestationId: Long): ResponseEntity<Map<String, List<FavoritePrestationDTO>?>>{
+    suspend fun getByPrestationFavorite(@PathVariable prestationId: Long): ResponseEntity<Map<String, List<FavoritePrestationDTO>?>>{
         val find = service.findByPrestationId(prestationId)
         return ResponseEntity.ok(mapOf("data" to find))
     }
@@ -77,15 +77,15 @@ class PrestationFavoriteController(
        return ResponseEntity.ok(mapOf("data" to find))
     }
 
-    @GetMapping("/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    suspend fun deleteById(@PathVariable id: Long): ResponseEntity<Map<String, String>>{
+    @DeleteMapping("/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    suspend fun deleteByIdFavoritePrestation(@PathVariable id: Long): ResponseEntity<Map<String, String>>{
         val find = service.findById(id)
         if (find != null) service.deleteById(find.favorite.id!!) else return ResponseEntity.badRequest().body(mapOf("error" to "favorite not found"))
         return ResponseEntity.ok().body(mapOf("message" to "Favorite deleted"))
     }
 
     @DeleteMapping("/delete/all")
-    suspend fun deleteAll(): ResponseEntity<Map<String, String>>{
+    suspend fun deleteAllFavoritePrestation(): ResponseEntity<Map<String, String>>{
         service.deleteAll()
         return ResponseEntity.ok(mapOf("message" to "All favorite prestation deleted"))
     }
