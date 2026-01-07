c9edfdcf3390a9924213f7ee5721d475034a075b
ElieOko <elieoko100@gmail.com>
2026-01-07 16:13:29 +0100

    feat:implementation d'un nouveau filtre

--- src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/PropertyRepository.kt
+++ src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/PropertyRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.data.repository.query.Param
 import server.web.casa.app.property.infrastructure.persistence.entity.PropertyEntity
+import server.web.casa.app.property.infrastructure.persistence.entity.SalleFestiveEntity
 
 interface PropertyRepository : CoroutineCrudRepository<PropertyEntity, Long> {
 //    @EntityGraph(attributePaths = [
@@ -15,6 +16,26 @@ interface PropertyRepository : CoroutineCrudRepository<PropertyEntity, Long> {
 //        "propertyImageLivingRoom",
 //        "propertyImageKitchen"])
 override fun findAll(): Flow<PropertyEntity>
+    @Query("""
+        SELECT * FROM properties
+        WHERE transaction_type = :transactionType
+        AND price BETWEEN :minPrice AND :maxPrice
+        AND ((:city IS NULL AND (:cityValue IS NULL OR city_value = :cityValue) ) OR city_id = :city)
+        OR(rooms = :room)
+        OR ((:commune IS NULL AND (:communeValue IS NULL OR commune_value = :communeValue)) OR commune_id = :commune)
+        AND property_type_id = :typeMaison
+    """)
+    fun filter(
+        @Param("transactionType") transactionType: String,
+        @Param("minPrice") minPrice: Double,
+        @Param("maxPrice") maxPrice: Double,
+        @Param("city") city: Long?,
+        @Param("cityValue") cityValue: String?,
+        @Param("commune") commune: Long?,
+        @Param("communeValue") communeValue: String?,
+        @Param("typeMaison")  typeMaison: Long,
+        @Param("room")  room: Int,
+    ) : Flow<PropertyEntity>
 
     @Query("""
         SELECT r FROM PropertyEntity r 
