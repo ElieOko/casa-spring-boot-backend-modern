8926b4963167d6ee85f1aa7ea63d7f423fa54992
ElieOko <elieoko100@gmail.com>
2026-01-07 16:12:06 +0100

    feat:creation et implementation du filtre

---
+++ src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/TerrainRepository.kt
@@ -0,0 +1,29 @@
+package server.web.casa.app.property.infrastructure.persistence.repository
+
+import kotlinx.coroutines.flow.Flow
+import org.springframework.data.r2dbc.repository.Query
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.data.repository.query.Param
+import server.web.casa.app.property.infrastructure.persistence.entity.*
+
+interface TerrainRepository  : CoroutineCrudRepository<TerrainEntity, Long> {
+    @Query("""
+        SELECT * FROM terrains
+        WHERE transaction_type = :transactionType
+        AND price BETWEEN :minPrice AND :maxPrice
+        AND ((:city IS NULL AND (:cityValue IS NULL OR city_value = :cityValue) ) OR city_id = :city)
+        OR(rooms = :room)
+        OR ((:commune IS NULL AND (:communeValue IS NULL OR commune_value = :communeValue)) OR commune_id = :commune)
+        AND property_type_id = :typeMaison
+    """)
+    fun filter(
+        @Param("transactionType") transactionType: String,
+        @Param("minPrice") minPrice: Double,
+        @Param("maxPrice") maxPrice: Double,
+        @Param("city") city: Long?,
+        @Param("cityValue") cityValue: String?,
+        @Param("commune") commune: Long?,
+        @Param("communeValue") communeValue: String?,
+        @Param("typeMaison")  typeMaison: Long,
+    ) : Flow<TerrainEntity>
+}
