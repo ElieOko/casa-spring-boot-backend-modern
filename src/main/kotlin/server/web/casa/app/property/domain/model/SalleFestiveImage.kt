f67d317b515e6bc459b4b389e066cb3f9c4b9ffe
ElieOko <elieoko100@gmail.com>
2026-01-07 16:14:44 +0100

    feat:nouveau model

--- src/main/kotlin/server/web/casa/app/property/domain/model/SalleFestiveImage.kt
+++ src/main/kotlin/server/web/casa/app/property/domain/model/SalleFestiveImage.kt
@@ -1,6 +1,7 @@
 package server.web.casa.app.property.domain.model
 
 import server.web.casa.app.property.infrastructure.persistence.entity.SalleFestiveImageEntity
+import server.web.casa.app.property.infrastructure.persistence.entity.TerrainImageEntity
 import java.time.LocalDateTime
 
 data class SalleFestiveImage(
@@ -19,4 +20,21 @@ fun SalleFestiveImage.toEntity()= SalleFestiveImageEntity(
     isAvailable = this.isAvailable,
     createdAt = this.createdAt,
     salleFestiveId = this.salleFestiveId
+)
+
+data class TerrainImage(
+    val id: Long? = null,
+    val terrainId : Long,
+    val name: String,
+    val path: String?,
+    var isAvailable: Boolean = true,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+)
+fun TerrainImage.toEntity()= TerrainImageEntity(
+    id = this.id,
+    name = this.name,
+    path = this.path,
+    isAvailable = this.isAvailable,
+    createdAt = this.createdAt,
+    terrainId = this.terrainId
 )
--- src/main/kotlin/server/web/casa/app/property/domain/model/Terrain.kt
+++ src/main/kotlin/server/web/casa/app/property/domain/model/Terrain.kt
@@ -1,5 +1,9 @@
 package server.web.casa.app.property.domain.model
 
+import server.web.casa.app.address.domain.model.*
+import server.web.casa.app.payment.domain.model.Devise
+import server.web.casa.app.property.domain.model.dto.*
+import server.web.casa.app.property.infrastructure.persistence.entity.TerrainEntity
 import java.time.LocalDateTime
 
 class Terrain(
@@ -25,4 +29,59 @@ class Terrain(
     val latitude: Double? = null,
     val longitude: Double? = null,
     val createdAt: LocalDateTime = LocalDateTime.now(),
+)
+
+fun Terrain.toEntity() = TerrainEntity(
+    id = this.id,
+    userId = this.userId,
+    title = this.title,
+    description = this.description,
+    deviseId = this.deviseId,
+    price = this.price,
+    surface = this.surface,
+    address = this.address,
+    communeId = this.communeId,
+    quartierId = this.quartierId,
+    cityId = this.cityId,
+    countryValue = this.countryValue,
+    cityValue = this.cityValue,
+    postalCode = this.postalCode,
+    communeValue = this.communeValue,
+    isAvailable = this.isAvailable,
+    latitude = latitude,
+    longitude = longitude,
+    createdAt = createdAt,
+    transactionType = this.transactionType,
+    propertyTypeId = this.propertyTypeId,
+)
+
+fun Terrain.toDto() = TerrainMasterDTO(
+    terrain = TerrainDTO(
+        id = this.id,
+        userId = this.userId,
+        title = this.title,
+        description = this.description,
+        price = this.price,
+        surface = this.surface,
+        transactionType = this.transactionType,
+        isAvailable = this.isAvailable,
+        createdAt = this.createdAt,
+    ),
+    postBy = "",
+    address = AddressDTO(
+        address = this.address,
+        communeValue = this.communeValue,
+        quartierValue = this.quartierValue,
+        cityValue = this.cityValue,
+        countryValue = this.countryValue,
+        postalCode = this.postalCode,
+    ),
+    localAddress = LocalAddressDTO(
+        city = City(this.cityId, 0, ""),
+        commune = Commune(this.communeId, 0, emptyList(), ""),
+        quartier = Quartier(this.quartierId, "")
+    ),
+    geoZone = GeoDTO(this.latitude, this.longitude),
+    typeProperty = PropertyType(this.propertyTypeId!!, ""),
+    devise = Devise(id = this.deviseId!!, "", "", 0.0),
 )
