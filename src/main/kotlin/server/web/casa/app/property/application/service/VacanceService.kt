aeefcb03c59f0623f95fa09ce1465a6dc6923696
ElieOko <elieoko100@gmail.com>
2026-01-07 16:15:52 +0100

    feat:methode de base service vacance

--- src/main/kotlin/server/web/casa/app/property/application/service/VacanceService.kt
+++ src/main/kotlin/server/web/casa/app/property/application/service/VacanceService.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import org.springframework.stereotype.Service
+import server.web.casa.app.property.domain.model.Terrain
 import server.web.casa.app.property.domain.model.Vacance
 import server.web.casa.app.property.domain.model.toEntity
 import server.web.casa.app.property.infrastructure.persistence.entity.toDomain
@@ -21,4 +22,11 @@ class VacanceService(
        val result = repository.save(data.toEntity()).toDomain()
         result
     }
+
+    suspend fun update(p: Vacance) = coroutineScope {
+        val data = p.toEntity()
+        val result = repository.save(data)
+        result.toDomain()
+    }
+
 }
