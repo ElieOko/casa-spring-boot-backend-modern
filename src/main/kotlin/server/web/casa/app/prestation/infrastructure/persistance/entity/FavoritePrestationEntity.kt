ac7d49cd099b4573c8bf4a61f33fcc36a6a528e6
mr zou <a.tshiyanze@gmail.com>
2026-01-07 19:00:13 +0100

    migration cotation prestation, sollicitation request, entity et contrler

--- src/main/kotlin/server/web/casa/app/prestation/infrastructure/persistance/entity/FavoritePrestationEntity.kt
+++ src/main/kotlin/server/web/casa/app/prestation/infrastructure/persistance/entity/FavoritePrestationEntity.kt
@@ -2,8 +2,10 @@ package server.web.casa.app.prestation.infrastructure.persistance.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDateTime
 
+@Table("favorite_prestations")
 data class FavoritePrestationEntity(
     @Id
     @Column("id")
