494ea2bf348f1da5fcffe9a46f2ccd5e5429151d
ElieOko <elieoko100@gmail.com>
2026-01-07 16:17:55 +0100

    feat:nouveau fichier et structuration

---
+++ src/main/kotlin/server/web/casa/app/property/application/service/TerrainService.kt
@@ -0,0 +1,116 @@
+package server.web.casa.app.property.application.service
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.*
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import server.web.casa.app.address.application.service.*
+import server.web.casa.app.payment.application.service.DeviseService
+import server.web.casa.app.property.domain.model.Terrain
+import server.web.casa.app.property.domain.model.dto.*
+import server.web.casa.app.property.domain.model.dto.toDTO
+import server.web.casa.app.property.domain.model.filter.PropertyFilter
+import server.web.casa.app.property.domain.model.toEntity
+import server.web.casa.app.property.infrastructure.persistence.entity.*
+import server.web.casa.app.property.infrastructure.persistence.repository.*
+import server.web.casa.app.user.application.service.UserService
+import kotlin.collections.*
+import kotlin.collections.map
+
+@Service
+class TerrainService(
+    private val repository: TerrainRepository,
+    private val terrainImage: TerrainImageRepository,
+    private val devise: DeviseService,
+    private val cityService: CityService,
+    private val communeService: CommuneService,
+    private val quartierService: QuartierService,
+    private val userService: UserService,
+    private val propertyTypeService: PropertyTypeService,
+) {
+    suspend fun getAll() = coroutineScope{
+        val data =  repository.findAll().toList()
+        val terrain = mutableListOf<TerrainMasterDTO>()
+        val bureauIds: List<Long> = data.map { it.id!! }
+        val images = terrainImage.findByTerrainIdIn(bureauIds).toList()
+        val imageByTerrain: Map<Long, List<TerrainImageEntity>> = images.groupBy { it.terrainId }
+        data.forEach { m->
+            terrain.add(
+                TerrainMasterDTO(
+                    terrain = m.toDomain().toDTO(),
+                    images = imageByTerrain[m.id]?.map { it.toDomain() } ?: emptyList(),
+                    devise = devise.getById(m.deviseId!!),
+                    address = m.toAddressDTO(),
+                    localAddress = LocalAddressDTO(
+                        city = cityService.findByIdCity(m.cityId),
+                        commune = communeService.findByIdCommune(m.communeId),
+                        quartier = quartierService.findByIdQuartier(m.quartierId)
+                    ),
+                    geoZone = m.toGeo(),
+                    postBy = userService.findIdUser(m.userId).username,
+                    typeProperty = propertyTypeService.findByIdPropertyType(m.propertyTypeId?:0),
+                )
+            )
+        }
+        terrain
+    }
+    suspend fun getAllPropertyByUser(userId : Long) = coroutineScope{
+        val data = getAll().filter { it.terrain.userId == userId }.toList()
+        data
+    }
+    suspend fun create(data : Terrain) = coroutineScope {
+        repository.save(data.toEntity()).toDomain()
+    }
+    suspend fun findById(id : Long) = coroutineScope {
+        repository.findById(id)?.toDomain()?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Cette proprièté n'existe pas le terrain.")
+    }
+
+    suspend fun update(p: Terrain) = coroutineScope {
+        val data = p.toEntity()
+        val result = repository.save(data)
+        result.toDomain()
+    }
+    suspend fun filter(
+        filterModel : PropertyFilter,
+        page : Int,
+        size : Int,
+        sortBy : String,
+        sortOrder : String
+    ) = coroutineScope {
+        val terrain = mutableListOf<TerrainMasterDTO>()
+        val data = repository.filter(
+            transactionType = filterModel.transactionType,
+            minPrice = filterModel.minPrice,
+            maxPrice = filterModel.maxPrice,
+            city = filterModel.city,
+            commune = filterModel.commune,
+            typeMaison = filterModel.typeMaison,
+            cityValue = filterModel.cityValue,
+            communeValue = filterModel.communeValue,
+        ).toList()
+        val bureauIds: List<Long> = data.map { it.id!! }
+        val images = terrainImage.findByTerrainIdIn(bureauIds).toList()
+        val imageByTerrain: Map<Long, List<TerrainImageEntity>> = images.groupBy { it.terrainId }
+
+        data.forEach { m->
+            terrain.add(
+                TerrainMasterDTO(
+                    terrain = m.toDomain().toDTO(),
+                    images = imageByTerrain[m.id]?.map { it.toDomain() } ?: emptyList(),
+                    devise = devise.getById(m.deviseId!!),
+                    address = m.toAddressDTO(),
+                    localAddress = LocalAddressDTO(
+                        city = cityService.findByIdCity(m.cityId),
+                        commune = communeService.findByIdCommune(m.communeId),
+                        quartier = quartierService.findByIdQuartier(m.quartierId)
+                    ),
+                    geoZone = m.toGeo(),
+                    postBy = userService.findIdUser(m.userId).username,
+                    typeProperty = propertyTypeService.findByIdPropertyType(m.propertyTypeId?:0),
+                )
+            )
+        }
+        terrain
+    }
+}
