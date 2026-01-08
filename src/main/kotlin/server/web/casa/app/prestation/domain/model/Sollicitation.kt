0a895a85bfe4452ffc1ccc60418ffec51bb55413
mr zou <a.tshiyanze@gmail.com>
2026-01-08 12:46:11 +0100

    update to model DTO solliciatation

--- src/main/kotlin/server/web/casa/app/prestation/domain/model/Sollicitation.kt
+++ src/main/kotlin/server/web/casa/app/prestation/domain/model/Sollicitation.kt
@@ -1,6 +1,10 @@
 package server.web.casa.app.prestation.domain.model
 
+import server.web.casa.app.ecosystem.domain.model.Prestation
+import server.web.casa.app.payment.domain.model.Devise
+import server.web.casa.app.prestation.infrastructure.persistance.entity.SollicitationEntity
 import server.web.casa.app.reservation.domain.model.ReservationStatus
+import server.web.casa.app.user.domain.model.UserDto
 import java.time.LocalDate
 
 class Sollicitation(
@@ -8,11 +12,17 @@ class Sollicitation(
     val userId      : Long?,
     val prestationId: Long?,
     val deviseId    : Long?,
-    val budegt      : Double?,
+    val budget      : Double?,
     val description : String,
     var status      : String = ReservationStatus.PENDING.name,
     val startDate   : LocalDate,
     val endDate     : LocalDate,
     val createdAt   : LocalDate = LocalDate.now()
+)
 
+class SollicitationDTO(
+    val sollicitation: SollicitationEntity,
+    val user: UserDto,
+    val devise: Devise,
+    val prestation: Prestation
 )
