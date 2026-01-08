19b57d493be2215d1975f12e162902aeb2ec4a81
ElieOko <elieoko100@gmail.com>
2026-01-08 14:15:43 +0100

    feat:Hotel model compact

---
+++ src/main/kotlin/server/web/casa/app/property/domain/model/dto/HotelChambreDTO.kt
@@ -0,0 +1,13 @@
+package server.web.casa.app.property.domain.model.dto
+
+import server.web.casa.app.property.domain.model.*
+
+data class HotelChambreDTO(
+    val chambre : HotelChambre,
+    val image : List<HotelChambreImage>
+)
+
+data class HotelGlobal(
+    val hotel : Hotel,
+    val structure : List<HotelChambreDTO>
+)
