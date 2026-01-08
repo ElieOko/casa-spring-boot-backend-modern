a24960b026cca4893d1354f299261242ead1377e
ElieOko <elieoko100@gmail.com>
2026-01-08 14:30:32 +0100

    fix:review service

--- src/main/kotlin/server/web/casa/app/property/application/service/HotelChambreService.kt
+++ src/main/kotlin/server/web/casa/app/property/application/service/HotelChambreService.kt
@@ -1,11 +1,39 @@
 package server.web.casa.app.property.application.service
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.toList
 import org.springframework.stereotype.Service
+import server.web.casa.app.property.domain.model.HotelChambre
+import server.web.casa.app.property.domain.model.dto.HotelChambreDTO
+import server.web.casa.app.property.domain.model.toEntity
+import server.web.casa.app.property.infrastructure.persistence.entity.toDomain
+import server.web.casa.app.property.infrastructure.persistence.repository.HotelChambreImageRepository
 import server.web.casa.app.property.infrastructure.persistence.repository.HotelChambreRepository
+import kotlin.collections.map
 
 @Service
 class HotelChambreService(
-    private val repostory : HotelChambreRepository,
+    private val repository : HotelChambreRepository,
+    private val images : HotelChambreImageRepository,
 ) {
+    suspend fun create(model : HotelChambre) = coroutineScope { repository.save(model.toEntity()) }
 
+    suspend fun update(p: HotelChambre) = coroutineScope { repository.save(p.toEntity()).toDomain() }
+
+    suspend fun getAllChambreByHotel(hotel : Long) = coroutineScope{
+        val data = repository.getAllByHotel(hotel).toList()
+        val hotelList = mutableListOf<HotelChambreDTO>()
+        val hotelIds: List<Long> = data.map { it?.id!! }
+        val images = images.findByHotelChambreIdIn(hotelIds).toList()
+        val imageByBureau = images.groupBy { it.hotelChambreId }
+        data.forEach { ag->
+            hotelList.add(
+                HotelChambreDTO(
+                    chambre = ag!!.toDomain(),
+                    image = imageByBureau[ag.id]?.map { it.toDomain() } ?: emptyList()
+                )
+            )
+        }
+        hotelList
+    }
 }
--- src/main/kotlin/server/web/casa/app/property/application/service/HotelService.kt
+++ src/main/kotlin/server/web/casa/app/property/application/service/HotelService.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import org.springframework.stereotype.Service
 import server.web.casa.app.property.domain.model.Hotel
+import server.web.casa.app.property.domain.model.dto.HotelGlobal
+import server.web.casa.app.property.domain.model.dto.VacanceAgence
 import server.web.casa.app.property.domain.model.toEntity
 import server.web.casa.app.property.infrastructure.persistence.entity.toDomain
 import server.web.casa.app.property.infrastructure.persistence.repository.HotelRepository
@@ -15,6 +17,7 @@ import server.web.casa.utils.gcs.GcsService
 class HotelService(
     private val hotelRepository: HotelRepository,
     private val gcsService: GcsService,
+    private val chambre : HotelChambreService
 ) {
     suspend fun save(domain : Hotel) = coroutineScope {
         try {
@@ -27,6 +30,18 @@ class HotelService(
         }
     }
     suspend fun getAll() = coroutineScope { hotelRepository.findAll().map { it.toDomain() }.toList() }
+    suspend fun getAllHotel() = coroutineScope{
+        val hotel = mutableListOf<HotelGlobal>()
+        hotelRepository.findAll().collect {
+            hotel.add(
+                HotelGlobal(
+                    hotel = it.toDomain(),
+                    structure =  chambre.getAllChambreByHotel(it.id?:0)
+                )
+            )
+        }
+        hotel.toList()
+    }
     private suspend fun findAll() = coroutineScope{
 
     }
