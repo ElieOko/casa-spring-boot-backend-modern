11e0b9743547f21879d51e7b017a4ea470963b76
ElieOko <elieoko100@gmail.com>
2026-01-09 16:57:48 +0100

    feat:notification dto

--- src/main/kotlin/server/web/casa/app/notification/domain/model/NotificationSystem.kt
+++ src/main/kotlin/server/web/casa/app/notification/domain/model/NotificationSystem.kt
@@ -1,6 +1,7 @@
 package server.web.casa.app.notification.domain.model
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 data class NotificationSystem(
     val notificationSystemId : Long? = null,
@@ -8,3 +9,21 @@ data class NotificationSystem(
     val description : String,
     val dateCreated : LocalDate = LocalDate.now()
 )
+data class NotificationDTO(
+    val id: Long = 0,
+    val title: String,
+    val message: String,
+    val tag : String = TagType.RESERVATION.toString(),
+    val timestamp: LocalDateTime = LocalDateTime.now()
+)
+
+enum class TagType{
+    FINANCES,
+    SECURITY,
+    PROMOTIONS,
+    DEMANDES,
+    SYSTEMS,
+    RESERVATION,
+    AGENDA,
+    SESSION
+}
