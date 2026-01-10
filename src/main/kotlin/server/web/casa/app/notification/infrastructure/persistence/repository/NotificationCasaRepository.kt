5034c9836256fe1d3d6d689763273b27f96e0f03
ElieOko <elieoko100@gmail.com>
2026-01-10 13:44:56 +0100

    fix:add query for find user

--- src/main/kotlin/server/web/casa/app/notification/infrastructure/persistence/repository/NotificationCasaRepository.kt
+++ src/main/kotlin/server/web/casa/app/notification/infrastructure/persistence/repository/NotificationCasaRepository.kt
@@ -1,8 +1,12 @@
 package server.web.casa.app.notification.infrastructure.persistence.repository
 
+import kotlinx.coroutines.flow.Flow
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import server.web.casa.app.notification.infrastructure.persistence.entity.NotificationCasaEntity
+import server.web.casa.app.property.infrastructure.persistence.entity.AgenceEntity
 
 interface NotificationCasaRepository : CoroutineCrudRepository<NotificationCasaEntity, Long>{
-
+    @Query("SELECT * FROM notifications WHERE user_id = :userId AND is_active = true")
+    suspend fun getAllByUser(userId: Long) : Flow<NotificationCasaEntity?>
 }
