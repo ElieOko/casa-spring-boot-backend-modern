2a80f1c8bc210b9beafd266433ad02616d581758
ElieOko <elieoko100@gmail.com>
2026-01-10 11:45:51 +0100

    feat:ajout du service person et ajout de l'image de profil de celui qui post

--- src/main/kotlin/server/web/casa/app/property/application/service/HotelService.kt
+++ src/main/kotlin/server/web/casa/app/property/application/service/HotelService.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import org.springframework.stereotype.Service
-import server.web.casa.app.property.domain.model.Agence
+import server.web.casa.app.actor.application.service.PersonService
 import server.web.casa.app.property.domain.model.Hotel
 import server.web.casa.app.property.domain.model.dto.HotelGlobal
 import server.web.casa.app.property.domain.model.toEntity
@@ -17,6 +17,7 @@ import server.web.casa.utils.gcs.GcsService
 class HotelService(
     private val hotelRepository: HotelRepository,
     private val gcsService: GcsService,
+    private val person : PersonService,
     private val chambre : HotelChambreService
 ) {
     suspend fun save(domain : Hotel) = coroutineScope {
@@ -36,7 +37,8 @@ class HotelService(
             hotel.add(
                 HotelGlobal(
                     hotel = it.toDomain(),
-                    structure =  chambre.getAllChambreByHotel(it.id?:0)
+                    image = person.findByIdPersonUser(it.userId!!)?.images?:"",
+                    structure =  chambre.getAllChambreByHotel(it.id?:0),
                 )
             )
         }
@@ -52,6 +54,7 @@ class HotelService(
         data.map {it?.toDomain()}.toList().forEach { hotel.add(
             HotelGlobal(
                 hotel = it!!,
+                image = person.findByIdPersonUser(it.userId!!)?.images?:"",
                 structure =  chambre.getAllChambreByHotel(it.id?:0)
             )
         ) }
