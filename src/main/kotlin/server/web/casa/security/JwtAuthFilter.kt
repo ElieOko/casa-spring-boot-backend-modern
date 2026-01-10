4ba09d01da10db95dd1117b1381d5624d1fe4899
ElieOko <elieoko100@gmail.com>
2026-01-10 14:05:51 +0100

    fix:add notification and filter routes

--- src/main/kotlin/server/web/casa/security/JwtAuthFilter.kt
+++ src/main/kotlin/server/web/casa/security/JwtAuthFilter.kt
@@ -63,8 +63,8 @@ class JwtAuthFilter(
             "/otp/**",
             "/reset/password",
             "/refresh",
-            "/notifications/**",
-            "/notifications",
+            "/api/notifications/**",
+            "/api/notifications",
             "/websocket/**", // ← IMPORTANT: WebSocket doit être public pour le handshake
         )
 
