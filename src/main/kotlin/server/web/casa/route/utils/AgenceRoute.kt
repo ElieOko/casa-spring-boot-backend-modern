de839a8837e260319a056e46e73cd65a68bc4154
ElieOko <elieoko100@gmail.com>
2026-01-10 13:42:50 +0100

    fix:new route

--- src/main/kotlin/server/web/casa/route/utils/AgenceRoute.kt
+++ src/main/kotlin/server/web/casa/route/utils/AgenceRoute.kt
@@ -6,6 +6,7 @@ object AgenceRoute {
     const val AGENCE = "${GlobalRoute.ROOT}/${AgenceFeature.AGENCE_PATH}"
     const val CARD_TYPE = "${GlobalRoute.ROOT}/${AgenceFeature.CARD_PATH}"
     const val DEVISE = "${GlobalRoute.ROOT}/${AgenceFeature.DEVISE}"
+    const val NOTIFICATION = "${GlobalRoute.ROOT}/notifications"
 }
 object AgenceFeature {
     const val AGENCE_PATH = "agences"
