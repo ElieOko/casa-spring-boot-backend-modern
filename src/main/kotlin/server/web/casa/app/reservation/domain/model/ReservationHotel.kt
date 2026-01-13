32ec86baabaf5bc5fb7abf1be37dc940f2f7c63c
mr zou <a.tshiyanze@gmail.com>
2026-01-13 17:22:28 +0100

    service repository model entity reservation salle hotel

---
+++ src/main/kotlin/server/web/casa/app/reservation/domain/model/ReservationHotel.kt
@@ -0,0 +1,31 @@
+package server.web.casa.app.reservation.domain.model
+
+import server.web.casa.app.property.domain.model.Hotel
+import server.web.casa.app.property.domain.model.SalleFestive
+import server.web.casa.app.property.domain.model.SalleFuneraire
+import server.web.casa.app.reservation.infrastructure.persistence.entity.ReservationFestiveEntity
+import server.web.casa.app.reservation.infrastructure.persistence.entity.ReservationFuneraireEntity
+import server.web.casa.app.reservation.infrastructure.persistence.entity.ReservationHotelEntity
+import server.web.casa.app.user.domain.model.UserDto
+import java.time.LocalDate
+
+data class ReservationHotel(
+    val reservationId: Long? = null,
+    val userId: Long,
+    val hotelId: Long,
+    val message: String? = "",
+    val status: String = ReservationStatus.PENDING.name,
+    val type: String = ReservationType.STANDARD.name,
+    val isActive: Boolean = true,
+    val reservationHeure: String? = "",
+    val cancellationReason: String? = "",
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val createdAt: LocalDate = LocalDate.now(),
+)
+
+data class ReservationHotelDTO(
+    val reservation: ReservationHotelEntity,
+   // val hotel: Hotel,
+    val user: UserDto
+)
