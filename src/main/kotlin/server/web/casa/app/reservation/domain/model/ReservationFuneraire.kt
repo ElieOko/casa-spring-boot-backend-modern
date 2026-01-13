5e7b90cb294de413bfe5f1ae5cc45d10620a4895
mr zou <a.tshiyanze@gmail.com>
2026-01-13 17:01:13 +0100

    service repository model entity reservation salle funeraire

---
+++ src/main/kotlin/server/web/casa/app/reservation/domain/model/ReservationFuneraire.kt
@@ -0,0 +1,29 @@
+package server.web.casa.app.reservation.domain.model
+
+import server.web.casa.app.property.domain.model.SalleFestive
+import server.web.casa.app.property.domain.model.SalleFuneraire
+import server.web.casa.app.reservation.infrastructure.persistence.entity.ReservationFestiveEntity
+import server.web.casa.app.reservation.infrastructure.persistence.entity.ReservationFuneraireEntity
+import server.web.casa.app.user.domain.model.UserDto
+import java.time.LocalDate
+
+data class ReservationFuneraire(
+    val reservationId: Long? = null,
+    val userId: Long,
+    val funeraireId: Long,
+    val message: String? = "",
+    val status: String = ReservationStatus.PENDING.name,
+    val type: String = ReservationType.STANDARD.name,
+    val isActive: Boolean = true,
+    val reservationHeure: String? = "",
+    val cancellationReason: String? = "",
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val createdAt: LocalDate = LocalDate.now(),
+)
+
+data class ReservationFuneraireDTO(
+    val reservation: ReservationFuneraireEntity,
+    val salle: SalleFuneraire,
+    val user: UserDto
+)
