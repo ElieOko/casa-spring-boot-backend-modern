7b8fee5dd2ac81299489c2b0b94322b902247431
ElieOko <elieoko100@gmail.com>
2026-01-22 11:20:02 +0100

    feat:add field actor

--- src/main/kotlin/server/web/casa/app/ecosystem/domain/model/PrestationDTO.kt
+++ src/main/kotlin/server/web/casa/app/ecosystem/domain/model/PrestationDTO.kt
@@ -1,7 +1,15 @@
 package server.web.casa.app.ecosystem.domain.model
 
+import server.web.casa.app.actor.domain.model.UserPerson
+
 data class PrestationDTOMaster(
     val prestation : Prestation,
     val image : List<PrestationImage?>,
+    val postBy : String
+)
+data class PrestationDTOMasterAdmin(
+    val prestation : Prestation,
+    val image : List<PrestationImage?>,
     val postBy : String,
+    val actor : UserPerson
 )
