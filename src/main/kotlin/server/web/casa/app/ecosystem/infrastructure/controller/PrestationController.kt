783d4cac2484b60c6a4e6e6242af184331998ed8
ElieOko <elieoko100@gmail.com>
2026-01-22 17:25:39 +0100

    feat:controle du flux entrant par autorisation

--- src/main/kotlin/server/web/casa/app/ecosystem/infrastructure/controller/PrestationController.kt
+++ src/main/kotlin/server/web/casa/app/ecosystem/infrastructure/controller/PrestationController.kt
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
 import kotlinx.coroutines.coroutineScope
+import org.slf4j.LoggerFactory
 import org.springframework.http.*
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 import server.web.casa.app.address.application.service.*
 import server.web.casa.app.ecosystem.application.service.*
@@ -15,6 +17,7 @@ import server.web.casa.app.ecosystem.domain.request.PrestationRequest
 import server.web.casa.app.payment.application.service.DeviseService
 import server.web.casa.app.user.application.service.UserService
 import server.web.casa.route.ecosystem.Service
+import server.web.casa.security.Auth
 import server.web.casa.utils.ApiResponse
 
 @Tag(name = "Prestation Service", description = "Gestion des prestations services")
@@ -27,8 +30,10 @@ class PrestationController(
     private val prestationService : PrestationService,
     private val cityService: CityService,
     private val communeService: CommuneService,
-    private val quartierService: QuartierService
+    private val quartierService: QuartierService,
+    private val auth: Auth,
 ) {
+    private val log = LoggerFactory.getLogger(this::class.java)
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE])
     suspend fun createPrestationService(
         @Valid @RequestBody request: PrestationRequest,
@@ -41,30 +46,29 @@ class PrestationController(
         val baseUrl = if (isProd) "${requestHttp.scheme}://${requestHttp.serverName}"  else  "${requestHttp.scheme}://${requestHttp.serverName}:${requestHttp.serverPort}"
         val images = request.images
         val service = request.prestation
-        val user = userService.findIdUser(service.userId)
-        val devise = deviseService.getById(service.deviseId)
+        userService.findIdUser(service.userId)
+        deviseService.getById(service.deviseId)
         if (images.isNotEmpty()){
             val entity = service.toDomain()
             val data = prestationService.create(entity)
-            images.forEach {
-                prestationImage.create(PrestationImage(prestationId = data.id!!, name = it.image), server = baseUrl)
-            }
-            val response = mapOf(
-                "message" to "Enregistrement réussie avec succès",
-            )
+            images.forEach { prestationImage.create(PrestationImage(prestationId = data.id!!, name = it.image), server = baseUrl) }
+            val response = mapOf("message" to "Enregistrement réussie avec succès",)
             return ResponseEntity.status(201).body(response)
         }
-        val response = mapOf(
-            "message" to "Précisez au moins une réalisation"
-        )
+        val response = mapOf("message" to "Précisez au moins une réalisation")
         return ResponseEntity.badRequest().body(response)
     }
 
     @Operation(summary = "Voir les Prestaions service")
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     suspend fun getAllPrestation() = coroutineScope {
-        val response = mapOf("prestations" to prestationService.getAllData().toList())
-        ResponseEntity.ok().body(response)
+        val user = auth.user()
+        val state: Boolean? = user?.second?.find{ true }
+        val message = when (state) {
+         true -> mapOf("prestations" to prestationService.getAllData2().toList())
+         false,null -> mapOf("prestations" to prestationService.getAllData().toList())
+        }
+        ResponseEntity.ok().body(message)
     }
 
     @Operation(summary = "Get Prestaion by ID")
