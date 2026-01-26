2aaa353cdc135cd26f544f0a63f3993181268e84
ElieOko <elieoko100@gmail.com>
2026-01-26 14:14:55 +0100

    feat(property): ajout update is_available par user

--- src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/PropertyRepository.kt
+++ src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/PropertyRepository.kt
@@ -1,13 +1,10 @@
 package server.web.casa.app.property.infrastructure.persistence.repository
 
 import kotlinx.coroutines.flow.Flow
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
+import org.springframework.data.r2dbc.repository.Modifying
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.data.repository.query.Param
 import server.web.casa.app.property.infrastructure.persistence.entity.PropertyEntity
-import server.web.casa.app.property.infrastructure.persistence.entity.SalleFestiveEntity
 
 interface PropertyRepository : CoroutineCrudRepository<PropertyEntity, Long> {
 //    @EntityGraph(attributePaths = [
