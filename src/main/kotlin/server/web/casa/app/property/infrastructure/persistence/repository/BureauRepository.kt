b97565c022030617c4cc524999bdfe06a7eaa015
ElieOko <elieoko100@gmail.com>
2026-01-26 14:17:50 +0100

    feat(property): ajout setUpdateIsAvailable (bureau)

--- src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/BureauRepository.kt
+++ src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/BureauRepository.kt
@@ -1,6 +1,7 @@
 package server.web.casa.app.property.infrastructure.persistence.repository
 
 import kotlinx.coroutines.flow.Flow
+import org.springframework.data.r2dbc.repository.Modifying
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.data.repository.query.Param
@@ -38,4 +39,12 @@ interface BureauRepository : CoroutineCrudRepository<BureauEntity, Long>{
         WHERE user_id = :userId
     """)
     fun findAllByUser(userId: Long):Flow<BureauEntity>
+
+    @Modifying
+    @Query(
+        """ UPDATE salle_festives
+    SET is_available = :state
+    WHERE user_id = :userId"""
+    )
+    suspend fun setUpdateIsAvailable(userId: Long, state: Boolean = false): Int
 }
