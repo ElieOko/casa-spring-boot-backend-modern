25dfb53e3e5ccf29b0110f95be684b1ad4338684
ElieOko <elieoko100@gmail.com>
2026-01-26 14:17:44 +0100

    feat(property): ajout setUpdateIsAvailable (salle funeraire)

--- src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/SalleFuneraireRepository.kt
+++ src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/SalleFuneraireRepository.kt
@@ -1,6 +1,7 @@
 package server.web.casa.app.property.infrastructure.persistence.repository
 
 import kotlinx.coroutines.flow.Flow
+import org.springframework.data.r2dbc.repository.Modifying
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.data.repository.query.Param
@@ -40,4 +41,12 @@ interface SalleFuneraireRepository : CoroutineCrudRepository<SalleFuneraireEntit
         WHERE user_id = :userId
     """)
     fun findAllByUser(userId: Long):Flow<SalleFuneraireEntity>
+
+    @Modifying
+    @Query(
+        """ UPDATE salle_festives
+    SET is_available = :state
+    WHERE user_id = :userId"""
+    )
+    suspend fun setUpdateIsAvailable(userId: Long, state: Boolean = false): Int
 }
