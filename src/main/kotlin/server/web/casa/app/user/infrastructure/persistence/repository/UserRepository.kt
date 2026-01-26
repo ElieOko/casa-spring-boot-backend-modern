42a430bfcef2087f04186cecb0d04288ac8b3db4
ElieOko <elieoko100@gmail.com>
2026-01-26 15:18:00 +0100

    refactor(user): filtre is_lock sur findById et ajout update isLock

--- src/main/kotlin/server/web/casa/app/user/infrastructure/persistence/repository/UserRepository.kt
+++ src/main/kotlin/server/web/casa/app/user/infrastructure/persistence/repository/UserRepository.kt
@@ -1,5 +1,6 @@
 package server.web.casa.app.user.infrastructure.persistence.repository
 
+import org.springframework.data.r2dbc.repository.Modifying
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.data.repository.query.Param
@@ -8,9 +9,16 @@ import server.web.casa.app.user.infrastructure.persistence.entity.UserEntity
 
 interface UserRepository : CoroutineCrudRepository<UserEntity, Long> {
 
-    @Query("SELECT * FROM users WHERE email = :identifier OR phone = :identifier")
+    @Query("SELECT * FROM users WHERE email = :identifier OR phone = :identifier AND is_lock = false")
    suspend fun findByPhoneOrEmail(identifier: String) : UserEntity?
 //    suspend fun findByEmailOrPhone(email: String, phone: String): Mono<UserEntity>?
 
-
+    @Query("SELECT * FROM users WHERE is_lock = false")
+    override suspend fun findById(id: Long): UserEntity?
+    @Modifying
+    @Query("""UPDATE users
+    SET is_lock = :lock
+    WHERE user_id = :userId"""
+    )
+    suspend fun isLock(userId: Long, lock: Boolean = true): Int
 }
