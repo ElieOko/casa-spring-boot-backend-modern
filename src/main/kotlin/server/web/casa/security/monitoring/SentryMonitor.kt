613318607512bb07cb277d7e65cad4e277267834
ElieOko <elieoko100@gmail.com>
2026-01-27 18:26:10 +0100

    chore: load Sentry DSN from config

--- src/main/kotlin/server/web/casa/security/monitoring/SentryMonitor.kt
+++ src/main/kotlin/server/web/casa/security/monitoring/SentryMonitor.kt
@@ -7,13 +7,12 @@ import jakarta.annotation.PostConstruct
 
 @Component
 class SentryInitializer(
-//    @Value("\${sentry.dsn}") private val dsn: String,
-//    @Value("\${spring.profiles.active:dev}") private val env: String
+    @Value("\${sentry.dsn}") private val dsn: String
 ) {
     @PostConstruct
     fun init() {
         Sentry.init { options ->
-            options.dsn = "https://89ff13c7d1fa0e5092487c4875f6d42e@o4510707626082304.ingest.de.sentry.io/4510707628114000"
+            options.dsn = dsn
         }
     }
 }
