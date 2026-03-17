6decc476bb262c5e58c51497b9b85c87e4e425d7
ElieOko <elieoko100@gmail.com>
2026-03-17 21:52:49 +0100

    feat:new request by notification custom

--- src/main/kotlin/server/web/casa/app/notification/domain/model/request/NotificationSystemRequest.kt
+++ src/main/kotlin/server/web/casa/app/notification/domain/model/request/NotificationSystemRequest.kt
@@ -1,6 +1,6 @@
 package server.web.casa.app.notification.domain.model.request
 
-import server.web.casa.app.reservation.infrastructure.persistence.entity.ReservationEntity
+import server.web.casa.app.reservation.infrastructure.persistence.entity.*
 import server.web.casa.app.user.infrastructure.persistence.entity.UserEntity
 
 data class NotificationSystemRequest(
@@ -10,6 +10,42 @@ data class NotificationSystemRequest(
 
 data class NotificationReservation(
     val reservation : ReservationEntity,
+    val guestUser : UserEntity,
+    val hostUser : UserEntity
+)
+
+data class NotificationReservationFestive(
+    val reservation : ReservationFestiveEntity,
+    val guestUser : UserEntity,
+    val hostUser : UserEntity
+)
+
+data class NotificationReservationFuneraire(
+    val reservation : ReservationFuneraireEntity,
+    val guestUser : UserEntity,
+    val hostUser : UserEntity
+)
+
+data class NotificationReservationHotel(
+    val reservation : ReservationChambreHotelEntity,
+    val guestUser : UserEntity,
+    val hostUser : UserEntity
+)
+
+data class NotificationReservationTerrain(
+    val reservation : ReservationTerrainEntity,
+    val guestUser : UserEntity,
+    val hostUser : UserEntity
+)
+
+data class NotificationReservationVacance(
+    val reservation : ReservationVacanceEntity,
+    val guestUser : UserEntity,
+    val hostUser : UserEntity
+)
+
+data class NotificationReservationBureau(
+    val reservation : ReservationBureauEntity,
     val guestUser : UserEntity,
     val hostUser : UserEntity
 )
