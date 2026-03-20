afa49489836adc59de0af1482c759f79417329e0
ElieOko <elieoko100@gmail.com>
2026-03-20 16:46:44 +0100

    fix:ajout du prefixe casanayo

--- src/main/kotlin/server/web/casa/utils/EncodageReference.kt
+++ src/main/kotlin/server/web/casa/utils/EncodageReference.kt
@@ -3,8 +3,17 @@ package server.web.casa.utils
 import java.security.MessageDigest
 import java.util.UUID
 
+//fun generateTransactionReference(): String {
+//    val input = UUID.randomUUID().toString() + System.nanoTime()
+//    val bytes = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
+//    return bytes.joinToString("") { "%02x".format(it) }
+//}
+
 fun generateTransactionReference(): String {
+    val prefix = "CasaNayo"
     val input = UUID.randomUUID().toString() + System.nanoTime()
     val bytes = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
-    return bytes.joinToString("") { "%02x".format(it) }
+    val hash = bytes.joinToString("") { "%02x".format(it) }
+
+    return prefix + hash.take(20 - prefix.length)
 }
