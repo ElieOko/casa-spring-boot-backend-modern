a21150ce15e15d38b6064568078096928b25f815
ElieOko <elieoko100@gmail.com>
2026-03-25 09:51:16 +0100

    fix:bug id not found by parrainId

--- src/main/kotlin/server/web/casa/app/actor/infrastructure/controller/master/PersonController.kt
+++ src/main/kotlin/server/web/casa/app/actor/infrastructure/controller/master/PersonController.kt
@@ -44,7 +44,7 @@ class PersonController(
             val accountItems = request.account
             if (accountItems.isEmpty()) throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Choisissez au moins un type de comptes.")
             val account = accountItems.map { accountService.findByIdAccount(it.typeAccount)}.first()
-            if (request.actor.parrainId != null || request.actor.parrainId?.toInt() != 0) {
+            if (request.actor.parrainId != null && request.actor.parrainId?.toLong() != 0L) {
               userService.findIdUser(request.actor.parrainId!!)
             }
             val userSystem = request.toUser(request.user.phone)
