a6ebe6782a4face271619928b4de4de57ae7eec5
ElieOko <elieoko100@gmail.com>
2026-04-09 13:05:57 +0100

    feat:restructuration des models transaction et reponse

--- src/main/kotlin/server/web/casa/app/payment/domain/model/ResponseTransaction.kt
+++ src/main/kotlin/server/web/casa/app/payment/domain/model/ResponseTransaction.kt
@@ -1,11 +1,23 @@
 package server.web.casa.app.payment.domain.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.google.protobuf.Message
 
 data class ResponseTransaction(
+    @JsonIgnore
     val code : String?,
     val message: String?,
+    @JsonIgnore
     val orderNumber: String?
+)
+
+data class ResponseTransactionCard(
+    @JsonIgnore
+    val code : String?,
+    val message: String?,
+    @JsonIgnore
+    val orderNumber: String?,
+    val url : String?
 )
 
 data class TransactionCallBack(
--- src/main/kotlin/server/web/casa/app/payment/domain/model/TransactionCard.kt
+++ src/main/kotlin/server/web/casa/app/payment/domain/model/TransactionCard.kt
@@ -1,14 +1,20 @@
 package server.web.casa.app.payment.domain.model
 
+import org.jetbrains.annotations.NotNull
+
 data class TransactionCard(
-    var authorization : String,
-    val merchant : String,
+    var authorization : String="",
+    val merchant : String = "casanayo",
     val reference : String,
     val amount : String,
-    val currency : String,
-    val description : String,
-    val callback_url : String,
-    val approve_url : String,
-    val cancel_url : String,
-    val decline_url : String
+    val currency : String = "USD",
+    val description : String = "Abonnement sur la plateforme",
+    val callback_url : String = "https://migration.casanayo.com/api/v1/public/payment/card/callback",
+    val approve_url : String = "https://migration.casanayo.com/api/v1/public/payment/card/callback/approve",
+    val cancel_url : String = "https://migration.casanayo.com/api/v1/public/payment/card/callback/cancel",
+    val decline_url : String = "https://migration.casanayo.com/api/v1/public/payment/card/callback/decline"
+)
+data class TransactionCardRequest(
+    @NotNull
+    val deviseId : Long,
 )
